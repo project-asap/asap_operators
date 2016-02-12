f977e16e6ab67ac6b5f2a6bbf294311495195c8d
Karen Murphy <k.l.murphy@qub.ac.uk>
2016-02-12 13:40:00 +0000

    Revised version of compiler which takes Unige workflow description and extracts input/outputs into operators in their own right.  Compiles with kmeans, Needs further testing for the other workflows and different scenarios.

--- compiler/jsontocpp.py
+++ compiler/jsontocpp.py
@@ -65,7 +65,9 @@ g_operatorBoxMap = {}
 g_nodeBoxMap = {}
 g_nodeMap = {}
 g_argMap = {}
-
+g_edges = []
+g_nodeInEdges = {}
+g_nodeOutEdges = {}
 
 """
         SCHEMA CLASSES which hold data parsed from a 
@@ -162,34 +164,31 @@ class OpSpecification:
 """ Represents an operator box from the workflow description """
 class OperatorBox:
 
-    def __init__(self, box, nodeid, name, constraints):
+    def __init__(self, nodeid, name, constraints, computational):
         self.nodeid = nodeid
         self.name = name
         self.constraints = constraints
         self.inputs = []
         self.outputs = []
         var=0
-        numinputs = box["operator"]["constraints"]["input"]
+
+	if computational is True:
+	    if constraints["opSpecification"].get("algorithm"):
+	        self.opSpecification = OpSpecification(constraints["opSpecification"]["algorithm"],
+						       constraints["opSpecification"]["args"])
+	    else:
+	        self.opSpecification = OpSpecification(constraints["opSpecification"]["format"],
+						   constraints["opSpecification"]["args"])
+
+        numinputs = constraints["input"]
         for i in range(0,int(numinputs)):
-            newinputbox = InputBox(box["operator"]["constraints"]["input"+str(i)])
+            newinputbox = InputBox(constraints["input"+str(i)])
             self.inputs.append(newinputbox)
-        numoutputs = box["operator"]["constraints"]["output"]
+
+        numoutputs = constraints["output"]
         for i in range(0,int(numoutputs)):
-            newoutputbox = OutputBox(box["operator"]["constraints"]["output"+str(i)])
+            newoutputbox = OutputBox(constraints["output"+str(i)])
             self.outputs.append(newoutputbox)
-
-	self.opSpecification = OpSpecification(constraints["opSpecification"]["algorithm"],
-						constraints["opSpecification"]["args"])
-
-"""
-class OperatorBox:
-
-    def __init__(self, name, args, binput, boutput):
-        self.name = name
-        self.args = args
-        self.binput = binput
-        self.boutput = boutput
-"""
 
 """ Represents a node operator box from the workflow description """
 class Node:
@@ -197,60 +196,123 @@ class Node:
     def __init__(self, taskids, name):
         self.taskids = taskids
         self.name = name
+""" Represents an edge from one node to another from the workflow description """
+class Edge:
+
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
 
 
 """   
-    THIS VERSION USES UNIGE's WORKFLOW DESCRIPTION FORMAT
+    USES UNIGE's WORKFLOW DESCRIPTION FORMAT
     Function to load workflow data from json into workflow classes
     and create and store a dictionary of instances of each player 
     (operator, input or output) by id.
 """
-def loadWorkflowDataTEST(data):
+def loadWorkflowData(data):
+
+    global g_edges
+    g_edges = data["workflow"]["edges"]
+
     for box in data["workflow"]["nodes"]:
         newnode = Node(box["taskids"],
                        box["name"])
         g_nodeMap[box["id"]] = newnode
 
     for box in data["workflow"]["tasks"]:
-        newoperatorbox = OperatorBox(box, 
+        newoperatorbox = OperatorBox( 
                                  box["nodeId"],
                                  box["name"],
-                                 # todo make input and output loops for multiple i/o's
-                                 box["operator"]["constraints"])
-        newinputbox = InputBox(box["operator"]["constraints"]["input0"])
-                                 # box["operator"]["constraints"]["input0_type"])
-        newoutputbox = OutputBox(box["operator"]["constraints"]["output0"])
-                                 # box["operator"]["constraints"]["output0_type"])
-        g_operatorBoxMap[box["id"]] = newoperatorbox
+                                 box["operator"]["constraints"],
+				 True)
+
+	"""
+		Here we are transforming Unige's workflow so each input/output is an 
+		oprator in its own right.  This makes it possible/easier to 
+		identify in-memory workflow optimisations
+		TODO - merge input and output processing, so 1 code section
+	"""
+        numinputs = box["operator"]["constraints"]["input"]
+        for i in range(0,int(numinputs)):
+            # newinputbox = InputBox(box["operator"]["constraints"]["input"+str(i)])
+            # self.inputs.append(newinputbox)
+
+	    """ Create new nodes for this input """
+	    newnodeid = 'i_'+box["id"]+'_'+str(i) 
+	    newtaskids = [newnodeid]
+	    newnodename = box["name"]
+            newnode = Node(newtaskids, newnodename)
+            g_nodeMap[newnodeid] = newnode
+
+	    """ Create new task for this input """
+	    newiooperatorbox = OperatorBox(newnodeid, newnodename,
+					box["operator"]["constraints"],
+					False)
+
+	    """ Create input edge to original operator """
+	    g_edges.append((newnodeid, box["nodeId"]))
+	    if not g_nodeInEdges:
+		g_nodeInEdges[box["nodeId"]] = [newnodeid]
+	    else:
+		g_nodeInEdges[box["nodeId"]].append(newnodeid)
+		
+            g_nodeBoxMap[newnodeid] = newiooperatorbox
+
+        numoutputs = box["operator"]["constraints"]["output"]
+        for i in range(0,int(numoutputs)):
+
+	    """ Create new nodes for this output """
+	    newnodeid = 'o_'+box["id"]+'_'+str(i) 
+	    newtaskids = [newnodeid]
+	    newnodename = box["name"]
+            newnode = Node(newtaskids, newnodename)
+            g_nodeMap[newnodeid] = newnode
+
+	    """ Create new task for this output """
+	    newiooperatorbox = OperatorBox(newnodeid, newnodename,
+					box["operator"]["constraints"],
+					False)
+
+	    """ Create input edge to original operator """
+	    g_edges.append((box["nodeId"], newnodeid))
+	    if not g_nodeOutEdges:
+		g_nodeOutEdges[box["nodeId"]] = [newnodeid]
+	    else:
+		g_nodeOutEdges[box["nodeId"]].append(newnodeid)
+
+            g_nodeBoxMap[newnodeid] = newiooperatorbox
+
+        # newinputbox = InputBox(box["operator"]["constraints"]["input0"])
+        # newoutputbox = OutputBox(box["operator"]["constraints"]["output0"])
 
         g_nodeBoxMap[box["nodeId"]] = newoperatorbox
 
-        # g_inputBoxMap[tuple(box["id"],box["operator"]["constraints"]["input"])] = newinputbox
-        # g_outputBoxMap[tuple(box["id"],box["operator"]["constraints"]["output"])] = newoutputbox
-        # g_inputNodeMap[tuple(box["nodeid"],box["operator"]["constraints"]["input"])] = newinputbox
-        # g_outputNodeMap[tuple(box["nodeid"],box["operator"]["constraints"]["output"])] = newoutputbox
 
-"""   
-    Function to load workflow data from json into workflow classes
-    and create and store a dictionary of instances of each player 
-    (operator, input or output) by id.
 """
-def loadWorkflowData(data):
-    for box in data["boxes"]:
-        if box["type"] == "input":
-            newinputbox = InputBox(box["name"],
-                                     box["filename"])
-            g_inputBoxMap[box["id"]] = newinputbox
-        elif box["type"] == "output":
-            newoutputbox = OutputBox(box["name"],
-                                     box["filename"])
-            g_outputBoxMap[box["id"]] = newoutputbox
-        elif box["type"] == "operator":
-            newoperatorbox = OperatorBox(box["name"],
-                                     box["args"],
-                                     box["input"],
-                                     box["output"])
-            g_operatorBoxMap[box["id"]] = newoperatorbox
+    create a new node and task, with id same as op's node id and task id but with
+    iX suffix (where X identifies which i/o the new node 
+    represents to/from the op (in the where case the op has multiple inputs).
+    Add the input specification to the new i/o operator node
+"""
+def createIOOperatorNode(origNode, origTaskId):
+
+
+    return newNode
+	
+""" Insert a node edge going from this new i/o op to the original op """
+def insertNewEdge(newOpNode, origNode):
+
+    return null
+
+""" Test for whether or not operator node is computational or input/output """
+def isComputationalNode(key):
+
+    task = g_nodeBoxMap[key]
+    if hasattr(task, "opSpecification"): 
+	return True
+    else:
+	return False
 
 """   
     Function to load materialised operator data from json into 
@@ -303,18 +365,10 @@ def loadOperatorLibraryData(data):
                 for algorithmType in operator["algorithm.types"]:
                     g_typedefMap[tuple([algorithmName, algorithmType])] = operator["types"]
         elif operator["type"] == "inout_declaration":
-            for algorithm in operator["algorithm.names"]:
-                numInputParams = len(g_operatorsMap[algorithm].constraint.inputConstraint)
-                for inputParam in range(0,numInputParams):
-                # for inputParam in g_operatorsMap[algorithm].constraint.inputConstraint:
-		    g_iodeclMap[tuple([algorithm,"input"+str(inputParam)])] = \
-                                                      operator["input"+str(inputParam)]
+            for dataset in operator["dataset.names"]:
+		g_iodeclMap[tuple([dataset,"input"])] = operator["input"]
+		g_iodeclMap[tuple([dataset,"output"])] = operator["output"]
 
-                numOutputParams = len(g_operatorsMap[algorithm].constraint.outputConstraint)
-                # for outputParam in g_operatorsMap[algorithm].constraint.outputConstraint:
-                for outputParam in range(0,numOutputParams):
-		    g_iodeclMap[tuple([algorithm,"output"+str(outputParam)])] = \
-                                                      operator["output"+str(outputParam)]
         elif operator["type"] == "arg_declaration":
             for algorithm in operator["algorithm.names"]:
 		g_argsdeclMap[algorithm] = operator["argTemplates"]
@@ -330,6 +384,42 @@ def loadOperatorLibraryData(data):
 """
 def tabPrint(str, tabcount, f):
     f.write(' ' * tabcount*4 + str)
+
+"""
+        Process/Generate IO declaration and initialisation statements
+"""
+def createIODeclaration(inEdge, ioId, declaredIOFiles, opcode, ioCount):
+
+    algName = g_nodeMap[inEdge].name
+    ioConstraints = g_operatorsMap[g_nodeMap[inEdge].name].constraint.inputConstraint
+    ioType = ioConstraints[ioCount].iotype
+    declarationTemplate = g_iodeclMap[tuple([ioType, ioId])]
+    
+    filename=""
+    if ioId is "input": 
+    	filename = g_nodeBoxMap[inEdge].inputs[ioCount].name 
+    else:
+    	filename = g_nodeBoxMap[inEdge].outputs[ioCount].name 
+    
+    ctr=0
+    if declarationTemplate is not None :
+	var = ioId+str(ioCount)
+
+	## Keep track of io files for condensing vars, 
+	## if it already exists use existing var handle
+	if declaredIOFiles.has_key(filename):
+	    prevVar = declaredIOFiles[filename]
+	    var = prevVar
+	else:
+	    declaredIOFiles[filename]=var
+	    declarationStr = declarationTemplate.replace("VARIN", var) \
+						.replace("VAROUT","\""+filename+"\"")
+	    tabPrint (declarationStr, 0, opcode)
+	    tabPrint ("\n", 0, opcode)
+	g_argMap[(algName,var)] = var
+	print "created argMap with ", algName, " and ", var
+        ctr += 1 
+
 
 """                              
         Beginning of main processing block
@@ -390,7 +480,9 @@ def main(argv):
     """ 
         Load workflow data from json
     """
-    loadWorkflowDataTEST(flow)
+    loadWorkflowData(flow)
+
+    # splitIOFromNodeTasks()
 
     """ DEBUG TRACE BLOCK
     print "-------------------------------- Operators -------------------------------------------- "
@@ -413,6 +505,8 @@ def main(argv):
     pprint(g_inputBoxMap)
     print "-------------------------------- output Boxes -------------------------------------------- "
     pprint(g_outputBoxMap)
+    print "-------------------------------- Edges -------------------------------------------- "
+    pprint(g_edges)
     """
 
     """ Set tab level for generated code """
@@ -441,9 +535,21 @@ def main(argv):
     """ For each node(task) in the workflow """
     for key in g_nodeMap:
 
+	print "1key is ", key
+	if isComputationalNode(key) is False:
+	    # The computational operator will drive the compiler
+	    continue
+
         """ 
             Open the output file for this operator 
         """
+
+	# This logic will needs to change -  If we are carrying out a merge
+	# then we actually want to rename the previous op code file
+	# to a combination of oplast_and_opcurrent eg.
+	# we could reaname oplast.cpp this new name structure
+	# and open it here, appending the code in this iteration
+
    	genDir = os.path.splitext(workflowfile)[0]+'.dir'
 	if not os.path.exists(genDir):
     		os.makedirs(genDir)
@@ -455,17 +561,14 @@ def main(argv):
         """
 
         # ASSUMPTION
-        # Isn't it safe to assume we should only ever have one task per node as Unige 
+        # We should only ever have one task per node as Unige 
         # documents state they optimise the workflow to make tasks nodes(vertices) in 
         # their own right ?  TODO fix if this turns out not to be the case
-        # Either way we will still loop round the tasks here for extensibility purposes
-	# so there may not be much to change.
+        # Either way we will still loop round the tasks here for extensibility 
 
         for i in range(0,len(g_nodeMap[key].taskids)):  # Should only be once if 1 node 
-						   	# per task in optimised workflow
-							# as per Unige document
+
             task = g_nodeMap[key].taskids[i]
-            # box = g_operatorBoxMap[task]
 
             """ print materialised header template to code output"""
             algName = g_nodeMap[key].name
@@ -480,68 +583,29 @@ def main(argv):
             STAGE 2
 	    Print variable declarations for input/output filenames
         """
-    	# for key in g_nodeMap:
 
         """ print materialised declarations from operator dictionary rules to code output"""
         algName = g_nodeMap[key].name
 
         tabPrint ("//  Variable Declarations holding input/output filenames  \n\n", 0, opcode)
 
-        for i in range(0,len(g_nodeMap[key].taskids)):  # Should only be once if 1 node per task
+
+        for i in range(0,len(g_nodeMap[key].taskids)):  # Should only be once if 1 task per node
 
             """ Input files IO """
-	    paramInfo = g_operatorsMap[g_nodeMap[key].name].constraint.inputConstraint
-	    numParams = len(paramInfo)
-	    for inputParam in range(0,numParams):
-                inputId = 'input'+str(inputParam)
-                declarationTemplate = g_iodeclMap[tuple([algName,inputId])]
 
-                filename = g_nodeBoxMap[key].inputs[inputParam].name 
+            ioCount = 0
+	    for inEdge in g_nodeInEdges[key]:
 
-                ctr=0
-                if declarationTemplate is not None :
-                        var = 'input'+str(inputParam)
-     
-                        ## Keep track of io files for condensing vars, 
-		        ## if it already exists use existing var handle
-                        if declaredIOFiles.has_key(filename):
-                            prevVar = declaredIOFiles[filename]
-                            var = prevVar
-                        else:
-                            declaredIOFiles[filename]=var
-                            declarationStr = declarationTemplate.replace("VARIN", var) \
-                                                       .replace("VAROUT","\""+filename+"\"")
-                            tabPrint (declarationStr, 0, opcode)
-                            tabPrint ("\n", 0, opcode)
-                        g_argMap[(algName,inputId)] = var
-                        ctr += 1 
+		createIODeclaration(inEdge, 'input', declaredIOFiles, opcode, ioCount)
+		ioCount += 1
 
-            """ Output files IO , TODO merge this section of code with input section above this """
-	    paramInfo = g_operatorsMap[g_nodeMap[key].name].constraint.outputConstraint
-	    numParams = len(paramInfo)
-	    for outputParam in range(0,numParams):
-                outputId = 'output'+str(outputParam)
-                declarationTemplate = g_iodeclMap[tuple([algName,outputId])]
+            ioCount = 0
+	    for outEdge in g_nodeOutEdges[key]:
 
-                filename = g_nodeBoxMap[key].outputs[outputParam].name 
+		createIODeclaration(outEdge, 'output', declaredIOFiles, opcode, ioCount)
+		ioCount += 1
 
-                ctr=0
-                if declarationTemplate is not None :
-                        var = 'output'+str(outputParam)
-     
-                        ## Keep track of io files for condensing vars, 
-		        ## if it already exists use existing var handle
-                        if declaredIOFiles.has_key(filename):
-                            prevVar = declaredIOFiles[filename]
-                            var = prevVar
-                        else:
-                            declaredIOFiles[filename]=var
-                            declarationStr = declarationTemplate.replace("VARIN", var) \
-                                                       .replace("VAROUT","\""+filename+"\"")
-                            tabPrint (declarationStr, 0, opcode)
-                            tabPrint ("\n", 0, opcode)
-                        g_argMap[(algName,outputId)] = var
-                        ctr += 1 
         tabPrint ("\n", 0, opcode)
 
         """ 
@@ -617,9 +681,16 @@ def main(argv):
                 tabPrint ("\n", tabcount, opcode)
             tabPrint ("\n", tabcount, opcode)
 
-            """ 
-                output input section 
             """
+                output input section
+            """
+	    # Only do this input section if we haven't identified an 'in-memory' merge from 
+	    # output of previous op
+	    # otherwise the earlier assigned variable (which holds the output from the 
+	    # previous operator) is effectively the input section 
+	    # in which case we can either re-assign the input variable or
+	    # change the name of the input variable to the earlier assignment in question
+
             tabPrint ("//  Input section \n\n", tabcount, opcode)
 
             """ NOTE: for now assume we have 1 input to operator...
@@ -640,7 +711,7 @@ def main(argv):
             tabPrint ("\n", tabcount, opcode)
 
             """ 
-                output operators call sequencec section 
+                print to code output the operators call sequencec section 
             """
             tabPrint ("//  Calling sequence section \n\n", tabcount, opcode)
             with open('templates/'+algName+'_'+'callsequence.template', 'r') as myfile:
@@ -658,6 +729,10 @@ def main(argv):
             """ 
                 output 'output' section 
             """
+	    # We will only do this if we haven't identified an in-memory merge situation
+ 	    # Otherwise we should record the variable name which holds the output for 
+	    # replacement of the next input variable
+
             tabPrint ("//  Output section \n\n", tabcount, opcode)
             paramInfo = g_operatorsMap[g_nodeMap[key].name].constraint.outputConstraint
             numParams = len(paramInfo)
@@ -680,6 +755,8 @@ def main(argv):
                 myfile.close()
             tabPrint(data, tabcount, opcode)
 
+	# We don't want to close this code file if we have identified a merge-optimisation
+	# Though we may rename it op1_and_op2
 
         opcode.close()
 
