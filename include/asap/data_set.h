3143efb401a9dbdbfb8c8ffa5cc7b971356e9913
Hans Vandierendonck <h.vandierendonck@qub.ac.uk>
2016-09-13 23:54:26 +0100

    Add documentation to source code

--- include/asap/data_set.h
+++ include/asap/data_set.h
@@ -49,48 +49,88 @@ struct select_vector_set_type<vector_type,
 
 }
 
-// A data set holding a list of vectors of type VectorTy and a list of names
-// identfying the coordinates/dimensions of the vectors.
+/** @brief A data set describes a list of vectors
+ *
+ * @details
+ * A data set holding a list of vectors of type VectorTy and a list of names
+ * identfying the coordinates/dimensions of the vectors.
+ *
+ * @tparam VectorTy The type of vectors, typically @ref asap::dense_vector or 
+ *         @ref asap::sparse_vector
+ * @tparam WordContTy The type of word container, see @ref asap/include/word_bank.h
+ * @tparam NameContTy The type of the container of words or column/row labels. Optional.
+ */
 template<typename VectorTy, typename WordContTy, typename NameContTy = void,
 	 typename = void>
 class data_set {
 public:
+    /** The type of vectors stored */
     typedef VectorTy 	 	 	 	 	vector_type;
+    /** The type of the container of row or column labels */
     typedef WordContTy 	 	 	 		word_container_type;
 
+    /** The type used to index a vector */
     typedef typename VectorTy::index_type	 	index_type;
+    /** The type of an element of a vector */
     typedef typename VectorTy::value_type	 	value_type;
+    /** A type tag to show if storage is managed by vector or by the data set */
     typedef typename VectorTy::memory_mgmt_type	 	memory_mgmt_type;
+    /** The memory allocator used by this class */
     typedef typename VectorTy::allocator_type	 	allocator_type;
 
+    /** The type of the primary container of labels */
     typedef word_container_type  		 	index_list_type;
+    /** The type of a list of vectors */
     typedef typename
     internal::select_vector_set_type<vector_type>::type	vector_list_type;
 
+    /** The type of a constant iterator over primary labels */
     typedef typename index_list_type::const_iterator	const_index_iterator;
+    /** The type of a constant iterator over vectors */
     typedef typename vector_list_type::const_iterator	const_vector_iterator;
+    /** The type of a non-constant iterator over vectors */
     typedef typename vector_list_type::iterator		vector_iterator;
 
 private:
-    // TODO: use VectorTy's allocator_type in both vectors?
+    /** @todo use VectorTy's allocator_type in both vectors? */
+    /** A name for the data set */
     const char            		* m_relation;
+    /** The list of primary labels */
     std::shared_ptr<index_list_type>	  m_idx_names;
+    /** The list of vectors */
     std::shared_ptr<vector_list_type>	  m_vectors;
 
 public:
+    /** Construct an empty data set */
     data_set() : m_relation( nullptr ) { }
+    /** @brief Construct a data set
+     * @param relation A name for the relation. The pointer must remain valid
+     *                 after the call
+     * @param idx_names The list of primary row/column labels
+     * @param vectors The list of vectors to store in the data set
+     */
     data_set( const char * relation,
 	      const std::shared_ptr<index_list_type> & idx_names,
 	      const std::shared_ptr<vector_list_type> & vectors )
 	: m_relation( relation ), m_idx_names( idx_names ),
 	  m_vectors( vectors ) { }
+    /** @brief Constructor for a data set with move semantics
+     * @param relation A name for the relation. The pointer must remain valid
+     *                 after the call
+     * @param idx_names The list of primary row/column labels
+     * @param vectors The list of vectors to store in the data set
+     */
     data_set( const char * relation,
 	      std::shared_ptr<index_list_type> && idx_names,
 	      std::shared_ptr<vector_list_type> && vectors )
 	: m_relation( relation ),
 	  m_idx_names( std::move( idx_names ) ),
 	  m_vectors( std::move( vectors ) ) { }
+    /** @brief The copy-constructor is disabled */
     data_set( const data_set & ds ) = delete;
+    /** @brief The move constructor
+     * @param ds The data set to move.
+     */
     data_set( data_set && ds ) 
 	: m_relation( ds.m_relation ),
 	  m_idx_names( std::move( ds.m_idx_names ) ),
@@ -98,27 +138,37 @@ public:
 	this->check();
     }
 
+    /** Data set destructor */
     ~data_set() { }
 
+    /** Return the number of primary labels in the data set */
     size_t get_dimensions() const {
 	return m_idx_names->size();
     }
+    /** Return the number of vectors in the data set */
     size_t get_num_points() const {
 	return m_vectors->size();
     }
+    /** Return the data set's name */
     const char * get_relation() const {
 	return m_relation;
     }
 
+    /** Return a pointer to the primary labels */
     const std::shared_ptr<index_list_type> & get_index_ptr() const {
 	return m_idx_names;
     }
+    /** Return reference to list of row/column names */
     const index_list_type & get_index() const { return *m_idx_names; }
+    /** Return reference to list of vectors in the data set */
     const vector_list_type & get_vectors() const { return *m_vectors; }
     
+    /** Return begin iterator over row/column names */
     const_index_iterator index_cbegin() const { return m_idx_names->cbegin(); }
+    /** Return begin iterator over row/column names */
     const_index_iterator index_cend() const { return m_idx_names->cend(); }
 
+    /** Return the name of a row/column */
     const char * get_index( size_t n ) const {
 	return (*m_idx_names)[n];
     }
--- include/asap/dense_vector.h
+++ include/asap/dense_vector.h
@@ -33,32 +33,59 @@
 
 namespace asap {
 
+/** @brief A dense vector
+ *
+ * @tparam IndexTy The type used to index the vector, typically an integer type
+ * @tparam ValueTy The type of the elements stored in the vector
+ * @tparam IsVectorized Whether to use vector (SIMD) operations
+ * @tparam MemoryMgmt Whether vectors own the associated heap memory
+ * @tparam Allocator The memory allocator used by this class
+ */
 template<typename IndexTy, typename ValueTy, bool IsVectorized,
 	 typename MemoryMgmt = mm_ownership_policy,
 	 typename Allocator = std::allocator<ValueTy>>
 class dense_vector
 {
 public:
+    /** Whether to use vector (SIMD) operations */
     static const bool is_vectorized = IsVectorized;
+    /** The type used to index the vector, typically an integer type */
     typedef IndexTy index_type;
+    /** The type of the elements stored in the vector */
     typedef ValueTy value_type;
+    /** Whether vectors own the associated heap memory */
     typedef MemoryMgmt memory_mgmt_type;
+    /** The memory allocator used by this class */
     typedef Allocator allocator_type;
+    /** The class holding the vector operations for this class */
     typedef dense_vector_operations<index_type, value_type, is_vectorized> vector_ops;
 
+    /** A tag class describing the characteristics of this class */
     struct _asap_tag : tag_dense, tag_vector { };
+    /** A dummy method definition to implement type inspection */
     void asap_decl(void);
 
 private:
+    /** Pointer to a dense array of values */
     value_type *m_value;
+    /** THe length of the vector */
     index_type m_length;
 
 private:
-    // length cannot be changed...
+    /** The default constructor is disabled.
+     * @details Length cannot be changed for a vector
+     */
     dense_vector() = delete; // : m_value(nullptr), m_length(0) { }
 public:
-    // Disallow this constructor in case vector does not do its own
-    // memory management.
+    /** Constructor for an empty vector with specified length.
+     *
+     * @details
+     * Disallow this constructor in case vector does not do its own
+     * memory management.
+     *
+     * @tparam MMT Whether the vector owns the heap memory
+     * @param length_ The length of the vector
+     */
     template<typename MMT = memory_mgmt_type,
 	     typename = typename std::enable_if<
 		 std::is_same<MMT, memory_mgmt_type>::value &&
@@ -69,6 +96,11 @@ public:
 	else
 	    m_value = nullptr;
     }
+    /** Assignment constructor
+     *
+     * @param value_ An array of values
+     * @param length_ The length of the vector
+     */
     dense_vector(value_type *value_, index_type length_) : m_length(length_) {
 	if( memory_mgmt_type::assign ) {
 	    m_value = allocator_type().allocate( m_length );
@@ -76,6 +108,10 @@ public:
 	} else
 	    m_value = value_;
     }
+    /** Move constructor
+     *
+     * @param pt The dense vector to move
+     */
     dense_vector(dense_vector &&pt) : m_length( pt.m_length ) {
 	if( memory_mgmt_type::assign_rvalue ) {
 	    m_value = allocator_type().allocate( m_length );
@@ -85,6 +121,10 @@ public:
 	    pt.m_value = nullptr; // necessary?
 	}
     }
+    /** Copy constructor
+     *
+     * @param pt The dense vector to copy
+     */
     dense_vector(const dense_vector &pt) : m_length( pt.m_length ) {
 	if( memory_mgmt_type::assign ) {
 	    m_value = allocator_type().allocate( m_length );
@@ -92,11 +132,13 @@ public:
 	} else
 	    m_value = pt.m_value;
     }
+    /** Destructor */
     ~dense_vector() {
 	if( m_value && memory_mgmt_type::deallocate )
 	    allocator_type().deallocate( m_value, m_length );
     }
 
+    /** Copy-assignment operator */
     dense_vector & operator = ( const dense_vector & dv ) {
 	// Cleanup
 	if( m_value && memory_mgmt_type::deallocate )
@@ -112,6 +154,7 @@ public:
 	return *this;
     }
 
+    /** Move-assignment operator */
     dense_vector & operator = ( dense_vector && dv ) {
 	if( m_value && memory_mgmt_type::deallocate )
 	    allocator_type().deallocate( m_value, m_length );
@@ -122,50 +165,65 @@ public:
 	return *this;
     }
 
+    /** Return the length of the vector */
     index_type length() const { return m_length; }
+    /** Return the dense array of values */
     const value_type * get_value() const { return m_value; }
 
+    /** Return a reference to specific element */
     value_type & operator[]( index_type idx ) {
 	return m_value[idx]; // unchecked
     }
-    // Assuming trivial types, copy is better than by reference
+    /** Return a specific element
+     * @details Assuming trivial types, copy is better than by reference
+     */
     value_type operator[]( index_type idx ) const {
 	return m_value[idx]; // unchecked
     }
 
+    /** Apply a functor to every element of the vector
+     * @param fn Functor to apply. Takes two arguments: the index and the value
+     */
     template<typename Fn>
     void map( Fn & fn ) {
 	for( index_type i=0; i < m_length; ++i )
 	    fn( i, m_value[i] );
     }
     
+    /** Normalize, i.e., scale by length of vector */
     void normalize(value_type n) {
 	vector_ops::scale( m_value, m_length, value_type(1)/n );
     }
+    /** Scale by the specific value */
     void scale(value_type alpha) {
 	vector_ops::scale( m_value, m_length, alpha );
     }
 
+    /** Set all elements to zero */
     void clear() {
 	vector_ops::set( m_value, m_length, value_type(0) );
 	clear_attributes();
     }
+    /** Clear the attributes of the vector (generic function) */
     void clear_attributes() {
     }
     
-    // Square of Euclidean distance
+    /** Return the square of Euclidean distance to another dense vector
+     *  with elements of the same type
+     */
     template<typename OtherVectorTy>
     typename std::enable_if<is_dense_vector<OtherVectorTy>::value, value_type>::type
     sq_dist(OtherVectorTy const& p) const {
 	return vector_ops::square_euclidean_distance( m_value, m_length, p.get_value() );
     }
     
-    // Square of Euclidean norm
+    /** Return the square of Euclidean distance of the vector to itself */
     value_type sq_norm() const {
 	return vector_ops::square_norm( m_value, m_length );
     }
     
-    // For reduction of centre computations
+    /** Element-wise vector addition with vector of same type
+     * Used in reduction of centre computations */
     template<typename OtherVectorTy>
     const typename std::enable_if<is_dense_vector<OtherVectorTy>::value, dense_vector>::type &
     operator += ( const OtherVectorTy & pt ) {
@@ -173,6 +231,7 @@ public:
 	return *this;
     }
 
+    /** Element-wise vector addition with sparse vector of same-type elements */
     template<typename OtherVectorTy>
     const typename std::enable_if<is_sparse_vector<OtherVectorTy>::value, dense_vector>::type &
     operator += ( const OtherVectorTy & pt ) {
@@ -185,15 +244,22 @@ public:
 	return *this;
     }
 
+    /** Copy vector attributes, if any */
     template<typename OtherVectorTy>
     typename std::enable_if<is_dense_vector<OtherVectorTy>::value>::type
     copy_attributes( const OtherVectorTy & pt ) { }
 };
 
 
-// A dense vector set with memory allocation optimized such that memory
-// is allocated only once for all vectors. This requires the use of
-// dense vectors without ownership of the vector data.
+/** @brief A set of dense vectors.
+ * 
+ * @detail
+ * A dense vector set with memory allocation optimized such that memory
+ * is allocated only once for all vectors. This requires the use of
+ * dense vectors without ownership of the vector data.
+ *
+ * @tparam VectorTy The type of dense vectors stored
+ */
 template<typename VectorTy>
 class dense_vector_set
 {
